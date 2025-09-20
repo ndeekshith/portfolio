511943aac78987da4f1c81841eec0cb9346d656b
Deekshith N <deekshithdeepith12042004@gmail.com>
2025-09-20 10:37:39 +0530

    Corrected the file path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { TypeAnimation } from 'react-type-animation';
-import resumePdf from '../assets/DataScientistResume.pdf';
+import resumePdf from '../assets/Resume.pdf';
 
 function Hero() {
     const containerVariants = {
